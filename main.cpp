946e75312f590937b9de47a0f58d7874cb07898a
lolledk <peter.ladefoged@outlook.com>
2025-02-28 07:51:14 +0100

    Added the ability to add task to the to-do list

--- main.cpp
+++ main.cpp
@@ -3,26 +3,52 @@
 #include <string>
 #include <vector>
 
+// Class representing a task
 class Task
 {
 	public:
 	std::string _description;
+
+	 // Constructor to initialize a task with a description
 	Task(std::string description){
 		_description = description;
 	}
 };
 
+// Class representing a to-do list
 class TodoList
 {
 	
 	public:
 	std::vector<Task> todolist;
+
+	// Method to add a task to the list
+	void addTask(const std::string & description) {
+		todolist.push_back(Task(description));
+	}
 	TodoList(){}
 };
 
 int main() {
 
     TodoList todo = TodoList();
+	std::string taskDescription;
+	char choice;
+
+	// Loop to allow the user to add multiple tasks
+	do {std::cout << "Enter task description: ";
+
+		std::getline(std::cin, taskDescription);
+
+		todo.addTask(taskDescription);
+
+		std::cout << "Do you want to add another task? (y/n): ";
+
+		std::cin >> choice;
+
+		std::cin.ignore();
+		
+	} while (choice == 'y' || choice == 'Y');
 
     return 0;
 }
